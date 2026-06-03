a005220636ff9860753061da67639025c1850b03
Abhishek Patel <apate320@charlotte.edu>
2026-06-03 15:14:39 -0400

    Changed folder structure

--- harvest-hub/backend/src/main/java/com/h_hub/harvesthub/HarvesthubApplication.java
+++ harvest-hub/backend/src/main/java/com/h_hub/harvesthub/HarvesthubApplication.java
@@ -2,8 +2,12 @@ package com.h_hub.harvesthub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan
 // @ComponentScan is optional as system will scan the entire package
 public class HarvesthubApplication {
 
--- harvest-hub/backend/src/main/java/com/h_hub/harvesthub/controller/ProductController.java
+++ harvest-hub/backend/src/main/java/com/h_hub/harvesthub/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.h_hub.harvesthub.service.IProductService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import com.h_hub.harvesthub.entity.Product;
 @RestController
 @RequestMapping("api/v1/products")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     private final IProductService iProductService;
